c31563677089dd581768155c2698d74b5f76f51d
Mehmet Koray Yılmaz <korpik123456@gmail.com>
2024-10-05 19:52:14 +0200

    camera shake + switching

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,8 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Cinemachine;
 public class CameraManager : MonoBehaviour
 {
-    
+    public static CameraManager instance;
+    public CinemachineVirtualCamera cam;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+    }
+    public void SwitchCameraFollowTarget(Transform target)
+    {
+        cam.Follow = target;
+    }
+
+
+    public void ShakeCamera(float duration,float magnitude)
+    {
+        StartCoroutine(Shake(duration, magnitude));
+    }
+
+
+    public IEnumerator Shake(float duration,float magnitude)
+    {
+        float passedTime = 0;
+
+        while (duration > passedTime)
+        {
+            cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = magnitude;
+            passedTime+= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+
+        }
+
+        cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0;
+
+    }
 }
